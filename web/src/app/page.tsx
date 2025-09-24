e02f1eac2e3ab5f3972b6a5d278e9149a9711fde
yasmine kharrat <yasminekharrat567@gmail.com>
2025-09-24 09:03:36 +0100

    test loki log driver

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -12,6 +12,7 @@ const Home = async ({ searchParams }: HomeProps) => {
   const page = searchParams.page ? Number(searchParams.page) : 1
   const { data: students } = await getStudents(page)
 
+  console.log('hello from home page to test loki logs')
   return (
     <div className="mx-auto mt-10 flex max-w-2xl flex-col items-center justify-center gap-8 px-4">
       <StudentTable students={students} />
